982d22600360efb30013de3969799237b10f9d0d
sonto.lau <sonto@ncsmatoMac-mini.local>
2014-06-27 15:41:12 +0800

    Initial Commit

---
+++ TBAS/TBASAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TBASAppDelegate.h
+//  TBAS
+//
+//  Created by sonto.lau on 14-6-27.
+//  Copyright (c) 2014年 sonto.lau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TBASAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TBAS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TBAS
+//
+//  Created by sonto.lau on 14-6-27.
+//  Copyright (c) 2014年 sonto.lau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TBASAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TBASAppDelegate class]));
+    }
+}
